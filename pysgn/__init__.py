e1ac2f3c56eb5c38dcd16bbd4d07817db68d67e9
Wang Boyu <boyu.wby@gmail.com>
2025-03-19 13:52:30 -0400

    update version number to 0.3.0

--- pysgn/__init__.py
+++ pysgn/__init__.py
@@ -25,7 +25,7 @@ __all__ = [
 ]
 
 __title__ = "pysgn"
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 __license__ = "MIT"
 _this_year = datetime.datetime.now(tz=datetime.timezone.utc).date().year
 __copyright__ = f"Copyright {_this_year} Wang Boyu"
